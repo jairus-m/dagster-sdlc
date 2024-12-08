57f7dbfbbc452f0204df4faf65ca25557afd32be
jairus-m <jairusmartinez@gmail.com>
2024-12-08 15:41:15 -0800

    update: include jobs/schedules in definitions __init__.py

--- dagster_proj/__init__.py
+++ dagster_proj/__init__.py
@@ -2,10 +2,14 @@ from dagster import Definitions, load_assets_from_modules
 
 from .assets import activities, dbt
 from .resources import database_resource, dbt_resource
+from .jobs import activities_update_job
+from .schedules import activities_update_schedule
 
 activities_assets = load_assets_from_modules([activities])
 analytics_dbt_assets = load_assets_from_modules(modules=[dbt]) 
 
+all_jobs = [activities_update_job]
+all_schedules = [activities_update_schedule]
 
 defs = Definitions(
     assets=activities_assets+ analytics_dbt_assets,
@@ -13,7 +17,6 @@ defs = Definitions(
         "database": database_resource,
         "dbt": dbt_resource,
     },
-    # jobs=all_jobs,
-    # schedules=all_schedules,
-    # sensors=all_sensors,
+    jobs=all_jobs,
+    schedules=all_schedules,
 )
