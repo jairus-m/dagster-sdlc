9f08b594eadcb7f4bc3af2c666fab675a9587880
jairus-m <jairusmartinez@gmail.com>
2024-12-14 08:21:01 -0800

    feat: add cheeky lil ML pipeline for energy prediction

--- dagster_proj/__init__.py
+++ dagster_proj/__init__.py
@@ -1,18 +1,19 @@
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import activities, dbt
+from .assets import activities, dbt, energy_prediction
 from .resources import database_resource, dbt_resource
 from .jobs import activities_update_job
 from .schedules import activities_update_schedule
 
 activities_assets = load_assets_from_modules([activities])
-analytics_dbt_assets = load_assets_from_modules(modules=[dbt]) 
+ml_assets = load_assets_from_modules([energy_prediction], group_name='ml_pipeline')
+analytics_dbt_assets = load_assets_from_modules([dbt], group_name='dbt_duckdb') 
 
 all_jobs = [activities_update_job]
 all_schedules = [activities_update_schedule]
 
 defs = Definitions(
-    assets=activities_assets+ analytics_dbt_assets,
+    assets=activities_assets+ml_assets+analytics_dbt_assets+ml_assets,
     resources={
         "database": database_resource,
         "dbt": dbt_resource,
---
+++ dagster_proj/assets/energy_prediction.py
@@ -0,0 +1,111 @@
+from dagster import (
+    asset, 
+    AssetKey, 
+    multi_asset, 
+    AssetOut,
+    MaterializeResult, 
+    MetadataValue, 
+    get_dagster_logger
+    )
+
+from dagster_duckdb import DuckDBResource
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+
+logger = get_dagster_logger()
+
+@asset(
+    deps=["fct_activities"],
+    compute_kind="DuckDB",
+)
+def cycling_data(database: DuckDBResource):
+    """Gets cycling data from DuckDB"""
+    query = """
+        select 
+            distance_miles as distance
+            , moving_time_minutes as moving_time
+            , total_elevation_gain_feet as total_elevation_gain
+            , average_speed_mph as average_speed
+            , average_watts
+            , weighted_average_watts
+            , kilojoules
+            , average_heartrate
+        from strava.obt_clean_activities
+        where sport_type in ('Virtual Ride', 'Ride')
+            and average_watts is not null
+    """
+    with database.get_connection() as conn:
+        df = conn.execute(query).fetch_df()
+    logger.info(f'Size of data: {df.shape}')
+    return df
+
+@multi_asset(outs={"training_data": AssetOut(), "test_data": AssetOut()})
+def preprocess_data(cycling_data):
+    """Preprocesses data (train/test split of X/y)"""
+    X = cycling_data.loc[:, ~cycling_data.columns.isin(['kilojoules'])]
+    y = np.sqrt(cycling_data.loc[:, cycling_data.columns.isin(['kilojoules'])]).values.ravel()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1998)
+    return (X_train, y_train), (X_test, y_test)
+
+@asset
+def sklearn_preprocessor():
+    """Implement Sklearn data preprocessor pipeline"""
+    sqrt_pipeline = Pipeline([
+        ('impute_regression', IterativeImputer(estimator=LinearRegression())),
+        ('sqrt_transform', FunctionTransformer(np.sqrt, feature_names_out='one-to-one')),
+        ('standard_scaler', StandardScaler())
+    ])
+
+    numeric_pipeline = Pipeline([
+        ('impute_regression', IterativeImputer(estimator=LinearRegression())),
+        ('standard_scaler', StandardScaler())
+    ])
+
+    return ColumnTransformer([
+        ('nums', numeric_pipeline, ['distance', 'moving_time', 'average_speed', 'average_watts', 'weighted_average_watts']),
+        ('sqrt_transform', sqrt_pipeline, ['total_elevation_gain'])
+    ], remainder='drop')
+
+@asset
+def random_forest_pipeline(sklearn_preprocessor):
+    """Sklearn preprocessor + random forest pipeline"""
+    return Pipeline([
+        ('preprocess', sklearn_preprocessor),
+        ('random_forest', RandomForestRegressor(n_estimators=100, random_state=1998)) # hehe :3
+    ])
+
+@asset
+def trained_model(training_data, random_forest_pipeline):
+    """Train the model"""
+    X_train, y_train = training_data
+    rf_pipeline = random_forest_pipeline
+    rf_pipeline.fit(X_train, y_train)
+    return rf_pipeline
+
+@asset
+def evaluate_model(trained_model, test_data):
+    """Predict and evaluate on testing data"""
+    X_test, y_test = test_data
+    y_pred = trained_model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    logger.info(f"Mean Squared Error: {mse}")
+    logger.info(f"R-squared Score: {r2}")
+    
+    return MaterializeResult(
+        metadata={
+            "mse": MetadataValue.float(mse),
+            "r2": MetadataValue.float(r2)
+        }
+    )
