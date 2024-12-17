d9e311fce88c144aa2d077b46aad190388b014f2
jairus-m <jairusmartinez@gmail.com>
2024-12-16 18:18:22 -0800

    feat: abstract strava API into it's own dagster resource

--- dagster_proj/resources/__init__.py
+++ dagster_proj/resources/__init__.py
@@ -3,6 +3,8 @@ from dagster_duckdb import DuckDBResource
 from dagster_dbt import DbtCliResource
 from ..project import dbt_project
 
+from .configured_resources import StravaAPIResource
+
 dbt_resource = DbtCliResource(
     project_dir=dbt_project,
     profiles_dir='analytics_dbt',
@@ -10,4 +12,10 @@ dbt_resource = DbtCliResource(
 
 database_resource = DuckDBResource(
     database=EnvVar("DUCKDB_DATABASE"),
+)
+
+strava_api_resouce = StravaAPIResource(
+    client_id=EnvVar("CLIENT_ID").get_value(),
+    client_secret=EnvVar("CLIENT_SECRET").get_value(),
+    refresh_token=EnvVar("REFRESH_TOKEN").get_value(),
 )
---
+++ dagster_proj/resources/configured_resources.py
@@ -0,0 +1,26 @@
+from dagster import ConfigurableResource, asset, Definitions
+import requests
+from typing import Dict, Any
+
+class StravaAPIResource(ConfigurableResource):
+    client_id: str
+    client_secret: str
+    refresh_token: str
+    _access_token: str = None
+
+    def get_access_token(self) -> str:
+        if not self._access_token:
+            self._access_token = self._refresh_access_token()
+        return self._access_token
+
+    def _refresh_access_token(self) -> str:
+        auth_url = "https://www.strava.com/oauth/token"
+        payload = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token': self.refresh_token,
+            'grant_type': 'refresh_token'
+        }
+        response = requests.post(auth_url, data=payload, timeout=100)
+        response.raise_for_status() 
+        return response.json()['access_token']
