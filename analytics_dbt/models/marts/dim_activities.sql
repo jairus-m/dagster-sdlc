98530b9ddc206e67927d08f45925b91692cea08b
jairus-m <jairusmartinez@gmail.com>
2024-12-21 13:16:26 -0800

    update: remove dim_dates + refactor mart layer + add tests

--- analytics_dbt/models/marts/dim_activities.sql
+++ analytics_dbt/models/marts/dim_activities.sql
@@ -1,12 +1,20 @@
 select 
-    id
+    id -- pk
     , name
     , has_heartrate
     , private
     , visibility
+    , sport_type as all_sport_types
     , case 
         when sport_type in ('VirtualRide', 'Ride', 'MountainBikeRide') then 'Cycling'
         when sport_type = 'Run' then 'Running'
         else 'Other'
     end as sport_type
+    -- date transforms
+    , date_trunc('week', strptime(date, '%m-%d-%Y')) AS week_start
+    , extract('dow' FROM strptime(date, '%m-%d-%Y')) + 1 AS day_of_week_num
+    , strftime(strptime(date, '%m-%d-%Y'), '%A') AS day_of_week
+    , extract('week' FROM strptime(date, '%m-%d-%Y')) AS week_num
+    , extract('month' FROM strptime(date, '%m-%d-%Y')) AS month_num
+    , strftime(strptime(date, '%m-%d-%Y'), '%B') AS month
 from {{ ref('obt_clean_activities') }}
