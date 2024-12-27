aaff120c0e12fccc2296e805d2d8509d02257fbb
jairus-m <jairusmartinez@gmail.com>
2024-12-27 08:36:01 -0800

    feat: add dynamic querying from snowflake/duckdb based on environment

--- dagster_proj/utils/__init__.py
+++ dagster_proj/utils/__init__.py
@@ -1,24 +1,28 @@
-from dagster import get_dagster_logger
+from dagster import get_dagster_logger, AssetExecutionContext
 import dlt
 from dlt import Pipeline
 
+import pandas as pd
+
 logger = get_dagster_logger()
+
 
 def dynamic_write_dlt(
     dagster_environment: str,
     duckdb_database_path: str,
     pipeline_name: str,
     dataset_name: str,
-) -> Pipeline: 
+) -> Pipeline:
     """
-    Based on the env var, DAGSTER_ENVIRONMENT, will create a 
+    Based on the env var, DAGSTER_ENVIRONMENT, will create a
     dlt Pipeline to write to either DuckDB (dev) or Snowflake (prod/branch)
     Args:
         dagster_environment (str): DAGSTER_ENVIRONMENT env var (prod, branch, dev)
         duckdb_database_path (str): DUCKDB_DATABASE_PATH env var
         pipeline_name (str): Name of dlt pipeline (for pipeline state/metadata)
         dataset_name: (str): Name of dlt dataset (becomes schema in the target DWH)
-
+    Returns:
+        dlt.Pipeline
     """
     if dagster_environment in ("prod", "branch"):
         logger.info("Writing to Snowflake..")
@@ -38,3 +42,32 @@ def dynamic_write_dlt(
         )
 
     return pipeline
+
+
+def dynamic_query(
+    dagster_environment: str,
+    context: AssetExecutionContext,
+    query: str,
+) -> pd.DataFrame:
+    """
+    Based on the env var, DAGSTER_ENVIRONMENT, will query table
+    from either DuckDB (dev) or Snowflake (prod/branch).
+    Args:
+        dagster_environment (str): DAGSTER_ENVIRONMENT env var (prod, branch, dev)
+        contect (AssetExecutionContext): context passed in Dagster to get database resource
+        query (str): SQL query
+    Returns:
+        pd.DataFrame
+    """
+    if dagster_environment == "dev":
+        with context.resources.database.get_connection() as conn:
+            df = conn.execute(query).fetch_df()
+    else:
+        with context.resources.database.get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                df = cursor.fetch_pandas_all()
+
+                df.columns = df.columns.str.lower()  # make sure col names are lowercase
+
+    return df
