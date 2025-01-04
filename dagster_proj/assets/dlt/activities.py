ff69fe4cce5659c9ee35e24dd123a5e924607f0b
jairus-m <jairusmartinez@gmail.com>
2025-01-04 13:01:10 -0800

    feat: add strava stats resource to dltHub assets + add dagster multi_asset

--- dagster_proj/assets/dlt/activities.py
+++ dagster_proj/assets/dlt/activities.py
@@ -1,25 +1,72 @@
 from dagster import (
-    asset,
+    multi_asset,
     EnvVar,
+    AssetOut,
+    Output,
     get_dagster_logger,
     AssetExecutionContext,
 )
 
 import dlt
 from dlt.sources.rest_api import RESTAPIConfig, rest_api_resources
+from dlt.sources.helpers.rest_client.paginators import SinglePagePaginator
 
 import pendulum
 
-from ...resources import StravaAPIResource
-from ...utils import dynamic_write_dlt
+from dagster_proj.resources import StravaAPIResource
+from dagster_proj.utils import dynamic_write_dlt
 
 logger = get_dagster_logger()
+
+
+def strava_activities():
+    """
+    dltHub RESTAPIConfig "resources" config for activities endpoint.
+    Args:
+        None
+    Returns:
+        dict[str, Any]
+    """
+    return {
+        "name": "activities",
+        "endpoint": {
+            "params": {
+                "after": {
+                    "type": "incremental",
+                    "cursor_path": "start_date_local",
+                    "initial_value": "2010-01-01 00:00:00+00:00",
+                    "convert": lambda dt_str: int(pendulum.parse(dt_str).timestamp()),
+                },
+            }
+        },
+        "primary_key": "id",
+        "write_disposition": "merge",
+    }
+
+
+def strava_stats(athlete_id: int):
+    """
+    dltHub RESTAPIConfig "resources" config for stats endpoint.
+    Args:
+        athlete_id (int): athlete id
+    Returns:
+        dict[str, Any]
+    """
+    return {
+        "name": "stats",
+        "endpoint": {
+            "path": f"athletes/{athlete_id}/stats",
+            "paginator": SinglePagePaginator(),
+        },
+        "write_disposition": "replace",
+    }
 
 
 @dlt.source
 def strava_rest_api_config(strava_resource: StravaAPIResource):
     logger.info("Extracting Strava data source")
     access_token = strava_resource.get_access_token()
+    athlete_id = strava_resource.athlete_id
 
     config: RESTAPIConfig = {
         "client": {
@@ -30,25 +77,7 @@ def strava_rest_api_config(strava_resource: StravaAPIResource):
             },
             "paginator": {"type": "page_number", "base_page": 1, "total_path": None},
         },
-        "resources": [
-            {
-                "name": "activities",
-                "endpoint": {
-                    "params": {
-                        "after": {
-                            "type": "incremental",
-                            "cursor_path": "start_date_local",
-                            "initial_value": "2010-01-01 00:00:00+00:00",
-                            "convert": lambda dt_str: int(
-                                pendulum.parse(dt_str).timestamp()
-                            ),
-                        },
-                    }
-                },
-                "primary_key": "id",
-                "write_disposition": "merge",
-            }
-        ],
+        "resources": [strava_activities(), strava_stats(athlete_id)],
     }
 
     logger.info("RESTAPIConfig set up, starting to yield resources...")
@@ -56,15 +85,22 @@ def strava_rest_api_config(strava_resource: StravaAPIResource):
     yield from rest_api_resources(config)
 
 
-@asset(
-    key=["strava", "activities"],
+@multi_asset(
+    outs={
+        "activities": AssetOut(
+            key=["strava", "activities"], description="Raw activity data from Strava."
+        ),
+        "stats": AssetOut(
+            key=["strava", "stats"], description="Summary athlete stats."
+        ),
+    },
     group_name="dltHub",
     required_resource_keys={"strava"},
 )
 def load_strava_activities(context: AssetExecutionContext):
     """
     dlt EL pipeline based off declarative Rest API Config
-    to load raw Strava activities into DuckDB
+    to load raw Strava activities/athlete stats
     """
     DUCKDB_DATABASE_PATH = EnvVar("DUCKDB_DATABASE").get_value()
     DAGSTER_ENVIRONMENT = EnvVar("DAGSTER_ENVIRONMENT").get_value()
@@ -82,3 +118,6 @@ def load_strava_activities(context: AssetExecutionContext):
     source = strava_rest_api_config(context.resources.strava)
     load_info = pipeline.run(source)
     logger.info(load_info)
+
+    yield Output(value=load_info[0], output_name="activities")
+    yield Output(value=load_info[1], output_name="stats")
